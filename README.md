264e46d7968c5e3a719b1ae14555fd8bee88ae49
saarif23 <arif23s.001@gmail.com>
2023-11-30 20:27:37 +0600

    added readme.md file

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 
-
-# admin email and password 
- `admin email :` arif@gmail.com
- `admin password :` Aa@123
-
+## Pet Adoption Platfrom 
+ # Animal Care Agency #
+ 
  # client side Live Link :
   `https://fantastic-muffin-b6e116.netlify.app/`
 # Server Side link :
   `https://pet-adoption-server-zeta.vercel.app/health`
 
   # Project Features
-  - 
+  - This is animal helping website . 
+  - Every person have donate in donate campaign  without login / signup 
+  - this website is resposive for mobile / laptop  and large device
+  - Use react hot toast and sweeit alart 2 for notification 
+  - use tanstack query for get data 
+  - use react select for all select category 
+  - use formik for add new pets and update and add campaign and update 
+  - use sweepier js  for slider 
+  - home page category, team member, photo galary and review component use sweepier js 
+  - dashboard layout use for admin and logged in user and if use / admin token is valid then user and admin can use dashboard pages
+  
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -40,24 +40,6 @@ const useAxiosSecure = () => {
 
 
 
-    // useEffect(() => {
-
-    //     axiosSecure.interceptors.response.use(res => {
-    //         return res;
-    //     }, error => {
-    //         console.log('axiossecure error', error.response)
-    //         // if (error.response.status === 401 || error.response.status === 403) {
-    //         //     console.log('logout the user')
-    //         //     logout()
-    //         //         .then(() => {
-    //         //             navigate("/login")
-
-    //         //         })
-    //         //         .catch(error => console.log(error))
-    //         // }
-    //     })
-    // }, [])
-
 
 
 
@@ -68,35 +50,3 @@ const useAxiosSecure = () => {
 
 export default useAxiosSecure;
 
-
-
-// \\
-// const { logout } = useContext(AuthContext);
-
-
-// axiosSecure.interceptors.response.use(function (response) {
-//     return response;
-// }, function (error) {
-//     const status = error.response.status;
-
-//     /// 401 and 403  status error user logout
-//     if (status === 401 || status === 403) {
-//         logout();
-//     }
-//     //
-//     useEffect(() => {
-//         instance.interceptors.response.use(res => {
-//             return res;
-//         }, error => {
-//             console.log('axiossecure error', error.response)
-//             if (error.response.status === 401 || error.response.status === 403) {
-//                 console.log('logout the user')
-//                 logout()
-//                     .then(() => {
-//                         navigate("/login")
-
-//                     })
-//                     .catch(error => console.log(error))
-//             }
-//         })
-//     }, [])
